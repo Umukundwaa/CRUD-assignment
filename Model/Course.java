30613b105e2da0f986cc02e5e2ba6315adf9f7ae
Umukundwaa <a.umukundwa1@alustudent.com>
2026-03-10 00:08:16 -0700

    Add Course class with properties and methods

---
+++ Model/Course.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class Course {
+    private int id;
+    private String courseName;
+    private String courseDescription;
+
+
+    public Course(int id, String courseName, String courseDescription) {
+        this.id = id;
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+
+    public int getId()
+    { return id; }
+    public void setId(int id)
+    {
+    this.id = id;}
+
+    public String getCourseName()
+    {
+        return courseName;
+    }
+    public void setCourseName(String courseName) {
+        this.courseName= courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+    public void setCourseDescription(String courseDescription) { this.courseDescription = courseDescription; }
+
+
+    @Override
+    public String toString() {
+        return "Course{id=" + id + ", Course name=" + courseName + " " +  ", Description name=" + courseDescription + "}";
+    }
+}
